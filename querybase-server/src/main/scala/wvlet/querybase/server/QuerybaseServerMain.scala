db721bc7c7d609aabc66ea6b0128fd6f5620dc55
Taro L. Saito <leo@xerial.org>
2020-06-24 15:12:18 -0700

    Shor version

--- querybase-server/src/main/scala/wvlet/querybase/server/QuerybaseServerMain.scala
+++ querybase-server/src/main/scala/wvlet/querybase/server/QuerybaseServerMain.scala
@@ -1,7 +1,8 @@
 package wvlet.querybase.server
 
 import wvlet.airframe.launcher.{Launcher, command, option}
-import wvlet.log.Logger
+import wvlet.log.{LogSupport, Logger}
+import wvlet.querybase.api.BuildInfo
 
 /**
   */
@@ -14,12 +15,13 @@ object QuerybaseServerMain {
 
 class QuerybaseServerMain(
     @option(prefix = "-h,--help", description = "Show help messages", isHelp = true) help: Boolean
-) {
+) extends LogSupport {
   @command(description = "Start Querybase server")
   def server(
       @option(prefix = "-p,--port", description = "port number (default: 8080)")
       port: Int = 8080
   ): Unit = {
+    info(s"Querybase version:${BuildInfo.version}")
 
     val config = QuerybaseServerConfig(port = port)
     QuerybaseServer
