904b21cb9e4d7b90372d99a0eed7646b1604e96c
Taro L. Saito <leo@xerial.org>
2021-03-04 00:28:04 -0800

    Fix layout

--- querybase-ui/src/main/scala/wvlet/querybase/ui/component/page/HomePage.scala
+++ querybase-ui/src/main/scala/wvlet/querybase/ui/component/page/HomePage.scala
@@ -1,7 +1,9 @@
 package wvlet.querybase.ui.component.page
 
+import wvlet.airframe.rx.Rx
 import wvlet.airframe.rx.html.RxElement
 import wvlet.airframe.rx.html.all._
+import wvlet.querybase.api.frontend.ServiceApi.ServiceNode
 import wvlet.querybase.ui.RPCService
 
 /**
@@ -11,26 +13,36 @@ trait HomePage extends RxElement with RPCService {
   override def render: RxElement =
     div(
       cls -> "px-1",
-      div(
-        style -> "height: calc(60vh - 60px);",
-        div("Hello Querybase")
-      ),
+//      div(
+//        style -> "height: calc(60vh - 60px);",
+//        div("Hello Querybase")
+//      ),
       table(
-        cls -> "table",
+        cls   -> "table table-dark my-1",
+        style -> "table-layout: fixed;",
         thead(
-          th("name"),
-          th("address"),
-          th("last heartbeat")
+          tr(
+            cls -> "bg-secondary",
+            th("name"),
+            th("address"),
+            th("last heartbeat")
+          )
         ),
-        rpcRx(_.ServiceApi.serviceNodes()).map { nodeList =>
-          nodeList.map { n =>
-            tr(
-              td(n.name),
-              td(n.address),
-              td(n.lastHeartBeatAt.toString)
-            )
+        Rx.intervalMillis(1000).flatMap { i =>
+            rpcRx(_.ServiceApi.serviceNodes())
+          }.filter(_.isDefined).map { opt =>
+            opt.map { nodeList =>
+              tbody(
+                nodeList.map { n =>
+                  tr(
+                    td(n.name),
+                    td(n.address),
+                    td(n.lastHeartBeatAt.toString)
+                  )
+                }
+              )
+            }
           }
-        }
       ),
       div(
         cls   -> "bg-dark",
