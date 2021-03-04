796fd9f508c8844a30f9f4c365b1b648276b9e30
Taro L. Saito <leo@xerial.org>
2021-03-04 12:49:41 -0800

    Update table layout

--- querybase-ui/src/main/scala/wvlet/querybase/ui/component/Table.scala
+++ querybase-ui/src/main/scala/wvlet/querybase/ui/component/Table.scala
@@ -1,17 +1,22 @@
 package wvlet.querybase.ui.component
 
-import wvlet.airframe.rx.html.all.{cls, table, tbody, th, thead, tr}
+import wvlet.airframe.rx.html.all.{cls, small, table, tbody, th, thead, tr}
 import wvlet.airframe.rx.html.{RxComponent, RxElement}
 
 class Table(columnNames: Seq[String]) extends RxElement {
   override def render: RxElement = {
     table(
-      cls -> "table table-sm table-hover",
+      cls -> "table table-sm table-hover border-right border-bottom",
       thead(
         cls -> "thead-light",
         tr(
           //cls -> "text-center",
-          columnNames.map { columnName => th(columnName) }
+          columnNames.map { columnName =>
+            th(
+              cls -> "font-weight-normal",
+              columnName
+            )
+          }
         )
       )
     )
